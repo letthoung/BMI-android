40786fc15a0d2ac3aeb846751d35cf22fac1eb5f
letthoung <thanhtung3496@gmail.com>
2020-07-13 17:09:14 -0400

    Added a separate activity to show the result

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.bmi
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.setBackgroundColor(Color.GREEN)
+
+        resultsTextView.visibility = View.INVISIBLE
+
+        calculateButton.setOnClickListener {
+            var weight = weightEditText.text.toString().toDouble();
+            var height = heightEditText.text.toString().toDouble();
+
+            var bmi = weight / (weight * height)
+            //resultsTextView.text = "BMI: " + String.format("%.2f",bmi)
+            //resultsTextView.visibility = View.VISIBLE
+        }
     }
 }
