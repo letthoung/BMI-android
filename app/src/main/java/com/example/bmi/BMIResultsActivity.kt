750a5e7f54633fb7e8847c3148d7596b5459b73e
Tung Le <thanhtung3496@gmail.com>
2020-07-14 00:36:40 -0400

    Added the result activity to show the BMI from intent receiver

--- app/src/main/java/com/example/bmi/BMIResultsActivity.kt
+++ app/src/main/java/com/example/bmi/BMIResultsActivity.kt
@@ -2,10 +2,16 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_bmiresults.*
 
 class BMIResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmiresults)
+    }
+    override fun onStart() {
+        super.onStart()
+        val result = intent.extras?.getString("result")
+        resultTextView.text = "BMI: " + result;
     }
 }
